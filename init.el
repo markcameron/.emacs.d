c36cebcf26fb5e775c17f01ad1de5ef9598bbc9e
Mark Cameron <mark.cameron@devfactory.ch>
2020-06-26 18:36:58 +0200

    Added groovy-mode

--- init.el
+++ init.el
@@ -172,7 +172,7 @@
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (yaml-mode json-mode dockerfile-mode yasnippet nyan-mode markdown-mode flycheck phpcbf anzu doom-modeline doom-themes material-theme color-theme-sanityinc-tomorrow monokai-theme editorconfig php-mode neotree web-mode)))
+    (groovy-mode yaml-mode json-mode dockerfile-mode yasnippet nyan-mode markdown-mode flycheck phpcbf anzu doom-modeline doom-themes material-theme color-theme-sanityinc-tomorrow monokai-theme editorconfig php-mode neotree web-mode)))
  '(pos-tip-background-color "#FFFACE")
  '(pos-tip-foreground-color "#000000")
  '(weechat-color-list
